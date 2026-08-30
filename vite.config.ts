15903d272b87c134842e695cd025facb1f278e00
Wanlin Cheung <chocomonster87@gmail.com>
2026-08-30 23:00:39 +0800

    update config to include other .html pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        // Point to all the HTML files you want to bundle into your final build
+        main: resolve(__dirname, "index.html"),
+        tia: resolve(__dirname, "project-tia.html"),
+        nyp: resolve(__dirname, "project-nyp.html"),
+        moh: resolve(__dirname, "project-moh.html"),
+      },
+    },
+  },
+});
